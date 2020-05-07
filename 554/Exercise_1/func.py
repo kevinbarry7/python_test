bbc65487790033bee7d546c36aab4b15e30f66e8
Kevin Barry <kevn.barry@gmail.com>
2020-05-07 18:30:18 -0400

    Commit 05072020_4

--- 554/Exercise_1/func.py
+++ 554/Exercise_1/func.py
@@ -24,23 +24,22 @@ def first_vowel(s):
     Precondition: s is a nonempty string with only lowercase letters
     """
 
-    def first_vowel(s):
+    result = len(s)
 
-        result = len(s)
+    if introcs.find_str(s, 'a') > -1:
+        return introcs.find_str(s, 'a')
+    if introcs.find_str(s, 'e') > -1:
+        return introcs.find_str(s, 'e')
+    if introcs.find_str(s, 'i') >= 0:
+        return introcs.find_str(s, 'i')
+    if introcs.find_str(s, 'o') >= 0:
+        return introcs.find_str(s, 'o')
+    if introcs.find_str(s, 'u') >= 0:
+        return introcs.find_str(s, 'u')
+    if introcs.find_str(s, 'y') > 0:
+        return introcs.find_str(s, 'y')
+    return result
 
-        if introcs.find_str(s, 'a') > -1:
-            return introcs.find_str(s, 'a')
-        if introcs.find_str(s, 'e') > -1:
-            return introcs.find_str(s, 'e')
-        if introcs.find_str(s, 'i') >= 0:
-            return introcs.find_str(s, 'i')
-        if introcs.find_str(s, 'o') >= 0:
-            return introcs.find_str(s, 'o')
-        if introcs.find_str(s, 'u') >= 0:
-            return introcs.find_str(s, 'u')
-        if introcs.find_str(s, 'y') > 0:
-            return introcs.find_str(s, 'y')
-        return result
 
-    place = first_vowel('sss')
-    print(place)
+place = first_vowel('sssa')
+print(place)
