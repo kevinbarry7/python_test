e72d9d10eb8d75d6a541ace327031a6dd4f665c5
Kevin Barry <kevn.barry@gmail.com>
2020-05-07 18:26:06 -0400

    Commit 05072020_3

--- 554/Exercise_1/func.py
+++ 554/Exercise_1/func.py
@@ -23,4 +23,24 @@ def first_vowel(s):
     Parameter s: the string to search
     Precondition: s is a nonempty string with only lowercase letters
     """
-    pass
+
+    def first_vowel(s):
+
+        result = len(s)
+
+        if introcs.find_str(s, 'a') > -1:
+            return introcs.find_str(s, 'a')
+        if introcs.find_str(s, 'e') > -1:
+            return introcs.find_str(s, 'e')
+        if introcs.find_str(s, 'i') >= 0:
+            return introcs.find_str(s, 'i')
+        if introcs.find_str(s, 'o') >= 0:
+            return introcs.find_str(s, 'o')
+        if introcs.find_str(s, 'u') >= 0:
+            return introcs.find_str(s, 'u')
+        if introcs.find_str(s, 'y') > 0:
+            return introcs.find_str(s, 'y')
+        return result
+
+    place = first_vowel('sss')
+    print(place)
